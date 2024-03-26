1bcefdb96a7b1341ec830724cd58b269fd3c5782
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-03-26 15:54:57 +0100

    add comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -13,6 +13,7 @@
         <tr>
           <th>Id</th>
           <th>Title</th>
+          <th>Synopsis</th>
           <th>Author</th>
           <th>Pencils</th>
           <th>Inks</th>
@@ -23,27 +24,20 @@
         </tr>
       </thead>
       <tbody>
-        @forelse($comics as $comic)
         <tr>
           <td>{{$comic->id}}</td>
           <td>{{$comic->title}}</td>
+          <td>{{$comic->synopsis}}</td>
           <td>{{$comic->author}}</td>
           <td>{{$comic->pencils}}</td>
           <td>{{$comic->inks}}</td>
           <td>{{$comic->cover_by}}</td>
           <td>{{$comic->rating}}</td>
           <td>{{$comic->pages}}</td>
-          <td><a href="{{ route('comics.show', $comic)}}">vedi altro</a></td>
         </tr>
-        @empty
-        <tr>
-          <td colspan="100%">no info</td>
-        </tr>
-        @endforelse
       </tbody>
 
     </table>
-    {{$comics->links()}}
     </div>
     
   </section>
